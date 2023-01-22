d6017d4d835704496a4fe6c7285e1e83304973bc
Marcin Łukomski <116206253+marcinlukomski@users.noreply.github.com>
2023-01-22 19:38:12 +0100

    Update client.c

--- client.c
+++ client.c
@@ -20,11 +20,18 @@ void PressKey(){
 
 void DisplayMenu(){
     printf("\nMenu:\n");
+    printf("0. Odswiez\n");
     printf("1. Wyswietl liste zalogowanych uzytkownikow\n");
     printf("2. Wyslij prywatna wiadomosc do uzytkownika\n");
+    printf("3. Wyslij wiadomosc do grupy\n");
+    printf("4. Wyslij wiadomosc publiczna\n");
     printf("5. Zablokuj uzytkownika\n");
     printf("6. Odblokuj uzytkownika\n");
-    printf("10. Wyloguj\n");
+    printf("7. Dolacz do grupy\n");
+    printf("8. Wyjdz z grupy\n");
+    printf("9. Zablokuj grupe\n");
+    printf("10. Odblokuj grupe\n");
+    printf("20. Wyloguj\n");
 }
 
 int ChooseOption(){
@@ -90,12 +97,108 @@ void SendMessageToClient(int queue){
         printf("Blad podczas wysylania wiadomosci\n");
 }
 
+void SendMessageToGroup(int queue){
+    char sendTo[groupname_length];
+    printf("Podaj docelowa grupe: ");
+    scanf("%s", sendTo);
+    printf("Tresc wiadomosci: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 3;
+    strcpy(mes.mfrom, user.uname);
+    strcpy(mes.mtogroup, sendTo);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad podczas wysylania wiadomosci\n");
+}
+
+void SendPublicMessage(int queue){
+    printf("Tresc wiadomosci: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 4;
+    strcpy(mes.mfrom, user.uname);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad podczas wysylania wiadomosci\n");
+}
+
+void JoinGroup(int queue){
+    printf("Podaj nazwe grupy: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 30;
+    strcpy(mes.mfrom, user.uname);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad \n");
+}
+
+void LeaveGroup(int queue){
+    printf("Podaj nazwe grupy: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 31;
+    strcpy(mes.mfrom, user.uname);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad \n");
+}
+
+void BlockGroup(int queue){
+    printf("Podaj nazwe grupy: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 32;
+    strcpy(mes.mfrom, user.uname);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad \n");
+}
+
+void UnblockGroup(int queue){
+    printf("Podaj nazwe grupy: ");
+    getchar();
+    scanf("%[^\n]s", mes.mtext);
+    mes.mtype = 33;
+    strcpy(mes.mfrom, user.uname);
+    mes.mid = 0;
+    msgsnd(queue, &mes, (sizeof(mes) - sizeof(long)), 0);
+    int receive = msgrcv(queue, &mes, (sizeof(mes) - sizeof(long)), server_mes, 0);
+    if(receive > 0)
+        printf("%s\n", mes.mtext);
+    else
+        printf("Blad \n");
+}
+
 void getMessage(int queue){
     while(msgrcv(queue, &mes, sizeof(mes) - sizeof(long), server_chat_mes, IPC_NOWAIT) > 0){
         if(mes.mid == 0)
-            printf("Nowa wiadomość od %s:\n%s\n", mes.mfrom, mes.mtext);
-        else
-            printf("Nowa wiadomosc od X dla grupy Y\n");
+            printf("%s: %s\n", mes.mfrom, mes.mtext);
+        else if (mes.mid == 1)
+            printf("[%s]%s: %s\n", mes.mtogroup, mes.mfrom, mes.mtext);
+        else if (mes.mid == 2)
+            printf("[public]%s: %s\n", mes.mfrom, mes.mtext);
     }
 }
 
@@ -150,6 +253,9 @@ int main(int argc, char* argv[]) {
             getMessage(queue);
             DisplayMenu();
             switch(ChooseOption()){
+            	case 0:{
+            	    break;
+            	 }
                 case 1:{ //lista zalogowanych
                     strcpy(mes.mfrom, user.uname);
                     mes.mtype = 1;
@@ -162,6 +268,16 @@ int main(int argc, char* argv[]) {
                     PressKey();
                     break;
                 }
+                case 3:{
+                    SendMessageToGroup(queue);
+                    PressKey();
+                    break;
+                    }
+                case 4:{
+                    SendPublicMessage(queue);
+                    PressKey();
+                    break;
+                    }
                 case 5:{
                     blockUser(queue);
                     PressKey();
@@ -172,7 +288,27 @@ int main(int argc, char* argv[]) {
                     PressKey();
                     break;
                 }
+                case 7:{
+                    JoinGroup(queue);
+                    PressKey();
+                    break;
+                    }
+                case 8:{
+                    LeaveGroup(queue);
+                    PressKey();
+                    break;
+                    }
+                case 9:{
+                    BlockGroup(queue);
+                    PressKey();
+                    break;
+                    }
                 case 10:{
+                    UnblockGroup(queue);
+                    PressKey();
+                    break;
+                    }
+                case 20:{
                     getMessage(queue);
                     strcpy(mes.mfrom, user.uname);
                     mes.mid = user.uid;
